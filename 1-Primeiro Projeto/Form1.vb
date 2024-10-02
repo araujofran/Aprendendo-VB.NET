024a67962438d676fefb8697e7ffafeaec146068
fferr <fferr@DESKTOP-5MV4H0C>
2024-10-02 19:30:15 -0300

    2ª aula com projeto 2

--- 1-Primeiro Projeto/Form1.vb
+++ 1-Primeiro Projeto/Form1.vb
@@ -1,11 +1,21 @@
 ﻿Public Class Form1
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_01.Click
 
-        MsgBox("Ola Mundo!!!")
+        MsgBox("Ola Mundo!!!", MsgBoxStyle.AbortRetryIgnore
+               )
 
 
     End Sub
---
+++ Criando Variáveis/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+    End Sub
+End Class
